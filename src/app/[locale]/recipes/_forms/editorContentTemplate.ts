9dc749c5055c683704b4baf7d21e7f89b7d32c52
Dominik <revertux@gmail.com>
2024-01-19 11:55:23 +0100

    feat(translate): change initial editor content based on current locale  (#10)

--- src/app/[locale]/recipes/_forms/editorContentTemplate.ts
+++ src/app/[locale]/recipes/_forms/editorContentTemplate.ts
@@ -1,21 +1,27 @@
-export const contetntMarkdown = `
+export const contentMarkdownEn = `
 ### Ingredients
 
-* Headings
-* Lists
-  * Unordered
-  * Ordered
-  * Check lists
-  * And nested ;)
-* Links
-* Bold/Italic/Underline formatting
-* Tables
-* Code block editors
-* And much more.
+* 
+* 
+* 
+* 
+* 
 
 ### Steps
 
-The block below is a live React component. You can configure multiple live code presets that specify the 
-available npm packages and the default imports. You can also specify a default component that will be rendered in
-the live code block.
+...
+`;
+
+export const contentMarkdownPl = `
+### Składniki
+
+* 
+* 
+* 
+* 
+* 
+
+### Kroki
+
+...
 `;
--- src/app/[locale]/recipes/create/CreateForm.tsx
+++ src/app/[locale]/recipes/create/CreateForm.tsx
@@ -1,23 +1,38 @@
 'use client';
 
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
 import { type MDXEditorMethods } from '@mdxeditor/editor';
 
 import { createRecipe } from '@/app/lib/recipe/actions';
 import { type CreateRecipeState } from '@/app/lib/recipe/shema';
 
-import { contetntMarkdown } from '../_forms/editorContentTemplate';
+import {
+  contentMarkdownEn,
+  contentMarkdownPl,
+} from '../_forms/editorContentTemplate';
 import BasicInfoForm from '../_forms/BasicInfoForm';
 import Editor from '../_forms/Editor';
 import { Button } from '@/components/ui/button';
-import { useScopedI18n } from '@/app/locales/client';
+import { useCurrentLocale, useScopedI18n } from '@/app/locales/client';
 
 function CreateForm() {
   const initialState = { message: '', error: {} };
   const editorRef = useRef<MDXEditorMethods | null>(null);
 
   const t = useScopedI18n('recipe');
+  const locale = useCurrentLocale();
+
+  const getMarkdownContent = useMemo(() => {
+    switch (locale) {
+      case 'en':
+        return contentMarkdownEn;
+      case 'pl':
+        return contentMarkdownPl;
+      default:
+        return contentMarkdownPl;
+    }
+  }, []);
 
   const createRecipeWithAdditionalInfo = async (
     state: CreateRecipeState,
@@ -38,7 +53,7 @@ function CreateForm() {
     >
       <h1 className="mb-4 text-4xl font-bold">{t('createTitle')}</h1>
       <BasicInfoForm state={state} />
-      <Editor markdown={contetntMarkdown} editorRef={editorRef} />
+      <Editor markdown={getMarkdownContent} editorRef={editorRef} />
       <p className="py-2 text-red-600">{state.message}</p>
       <SubmitButton content={t('create')} />
     </form>
